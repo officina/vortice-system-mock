6e98f847715b551db252d182a7b8c0d537e2a3b0
Andrea Maschio <andrea.maschio@gmail.com>
2018-09-04 18:03:54 +0200

    first mock

--- src/views/Api.vue
+++ src/views/Api.vue
@@ -2,11 +2,11 @@
   <div class="about">
     <h1>Backend Resources Demo</h1>
     <p>Click on the links below to fetch data from the Flask server</p>
-    <a href="" @click.prevent="fetchResource">Fetch</a><br/>
-    <a href="" @click.prevent="fetchSecureResource">Fetch Secure Resource</a>
+
+    <a href="" @click.prevent="fetchSecureResource">Fetch System info</a>
     <h4>Results</h4>
-    <p v-for="r in resources" :key="r.timestamp">
-      Server Timestamp: {{r.timestamp | formatTimestamp }}
+    <p v-for="r in resources" :key="r.systemId">
+      SystemId: {{r | json }}
     </p>
     <p>{{error}}</p>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + Flask App"/>
+    <HelloWorld msg="Welcome to Vortice Device Simulator"/>
   </div>
 </template>
 
